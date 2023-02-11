2c406d5a105cceab0661f01d677cd32a1a6eacd6
JonatasMSS <jonatas.miguelss@gmail.com>
2023-02-11 20:37:06 -0300

    feat:header component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,15 @@
 
 import '../src/styles/global.css';
-import Whatsapp from './assets/whatsapp.svg';
-import Instagram from './assets/instagram.svg';
-import Logo from './assets/Logo.svg';
+import { Header } from './components/Header';
+
 function App() {
   
   
 
   return (
-   <div className='h-screen w-screen bg-defaultBackground flex flex-col items-center '>
-      <div className='flex justify-between w-screen bg-zinc-800 p-4'>
-        <div className='flex gap-2 '>
-          <img src={Whatsapp} className='w-8'/>
-          <img src={Instagram} className='w-8'/>
-        </div>
-        <div>
-          <img src={Logo} alt="Logo do app" />
-        </div>
-        <div className='flex bg-softWhite'>
-          <span className='font'>Carrinho</span>
-        </div>
-      </div>
-   </div>
+    <div className='h-screen w-screen bg-defaultBackground flex flex-col items-center '>
+        <Header/>
+     </div>
   )
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+import Whatsapp from '../assets/whatsapp.svg';
+import Instagram from '../assets/instagram.svg';
+import Logo from '../assets/Logo.svg';
+import Carrinho from '../assets/shoppingcart.svg';
+export function Header(){
+    return(
+        <div className='flex justify-between w-screen bg-zinc-800 p-4'>
+          <div className='flex gap-2 '>
+            <img src={Whatsapp} className='w-8'/>
+            <img src={Instagram} className='w-8'/>
+          </div>
+          <div>
+            <img src={Logo} alt="Logo do app" className='w-max'/>
+          </div>
+          <div className='flex gap-1 p-2 rounded-lg bg-softWhite'>
+            <span className='font-roboto-condensed font-normal text-sm'>Carrinho</span>
+            <img src={Carrinho} alt="Carrinho" className='w-5' />
+          </div>
+        </div>
+    )
+}
