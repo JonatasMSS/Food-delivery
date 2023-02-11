5e509e0f36165653bbef1e06d1d0f7100a624896
JonatasMSS <jonatas.miguelss@gmail.com>
2023-02-11 18:45:13 -0300

    Inicio de Projeto

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import '../src/styles/global.css';
+
+function App() {
+  
+  
+
+  return (
+    <>
+      <div className='bg-black w-screen h-screen'>
+        <h2>Ola mundo!</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
