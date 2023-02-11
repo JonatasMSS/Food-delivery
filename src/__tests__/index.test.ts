844ac9ea4dd28e27c7a0c2f68113ed69fda3b09a
JonatasMSS <jonatas.miguelss@gmail.com>
2023-02-11 19:53:52 -0300

    refactor:removed temporally tests

--- src/__tests__/index.test.ts
+++
@@ -1,9 +0,0 @@
-import {helloWorld} from '../testF';
-
-describe('Testing first function',()=>{
-    test('helloWorldFunction',()=>{
-        expect(helloWorld()).toBe('hello world');
-    })    
-})
-
-
--- src/testF.ts
+++
@@ -1,5 +0,0 @@
-
-
-export function helloWorld():string{
-    return "hello world";
-}
