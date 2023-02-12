e82969b2331fae1e7cf8e19273c159b62e36e2f6
JonatasMSS <jonatas.miguelss@gmail.com>
2023-02-11 21:47:49 -0300

    feat:Food componente finished - TODO: added properties

--- src/components/Food.tsx
+++ src/components/Food.tsx
@@ -1,12 +1,20 @@
 import Eu from '../assets/Eu.jpeg';
+import { Hamburger } from "phosphor-react";
 
 export function Food() {
     return (
-        <div className="flex items-center gap-1 w-full bg-white rounded-lg border-2 border-zinc-400 my-3 p-2">
+        <div className="flex items-center justify-between w-full bg-white rounded-lg border-2 border-zinc-400 my-3 p-2">
 
-            <img src={Eu} alt="Eu" className='w-16 h-16 rounded-lg' />
+            <div className='w-16 h-16 rounded-lg'>
+                <Hamburger weight='fill' size={'wid'} />
+            </div>
 
-            FOOD NAME
+            <div className='flex w-32 flex-col items-start'>
+                <span className='font-roboto-condensed font-bold'>Pastel de carne</span>
+                <span className='font-roboto-condensed text-sm'>Ingredientes por aqui...</span>
+            </div>
+
+            <span className='font-roboto-condensed text-lg font-bold text-defaultRed '>RS 00,00</span>
         </div>
     );
 }
