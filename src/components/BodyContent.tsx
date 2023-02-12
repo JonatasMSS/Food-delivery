3e52cc2ce296d11483b57f9d84308d4c833de963
JonatasMSS <jonatas.miguelss@gmail.com>
2023-02-12 13:57:34 -0300

    refactor: refactored the responsive size grown

--- src/components/BodyContent.tsx
+++ src/components/BodyContent.tsx
@@ -6,7 +6,7 @@ export function BodyContent(){
     
 
     return(
-        <div className='bg-softWhite min-h-screen my-6 flex flex-col rounded-lg p-2 w-11/12'>
+        <div className='bg-softWhite my-6 flex flex-col rounded-lg p-2 w-11/12'>
         <span
           className='font-roboto-condensed text-xl font-bold border-b-2 border-b-zinc-500'
         >
@@ -22,60 +22,8 @@ export function BodyContent(){
           tastes={['Carne','Frango']}
 
         />
-        <Food 
-          foodImage={Eu}
-          name='Comida de teste'
-          ingredients={['Coxinha','pastel','carne','frango']}
-          price={8}
-          extras={['Extra 1','Extra 2']}
-          tastes={['Carne','Frango']}
-
-        />
-        <Food 
-          foodImage={Eu}
-          name='Comida de teste'
-          ingredients={['Coxinha','pastel','carne','frango']}
-          price={8}
-          extras={['Extra 1','Extra 2']}
-          tastes={['Carne','Frango']}
-
-        />
-        <Food 
-          foodImage={Eu}
-          name='Comida de teste'
-          ingredients={['Coxinha','pastel','carne','frango']}
-          price={8}
-          extras={['Extra 1','Extra 2']}
-          tastes={['Carne','Frango']}
-
-        />
-        <Food 
-          foodImage={Eu}
-          name='Comida de teste'
-          ingredients={['Coxinha','pastel','carne','frango']}
-          price={8}
-          extras={['Extra 1','Extra 2']}
-          tastes={['Carne','Frango']}
-
-        />
-        <Food 
-          foodImage={Eu}
-          name='Comida de teste'
-          ingredients={['Coxinha','pastel','carne','frango']}
-          price={8}
-          extras={['Extra 1','Extra 2']}
-          tastes={['Carne','Frango']}
-
-        />
-        <Food 
-          foodImage={Eu}
-          name='Comida de teste'
-          ingredients={['Coxinha','pastel','carne','frango']}
-          price={8}
-          extras={['Extra 1','Extra 2']}
-          tastes={['Carne','Frango']}
-
-        />
+       
+        
       </div>
     );
 }
