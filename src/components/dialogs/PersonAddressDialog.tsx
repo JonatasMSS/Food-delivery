6474cbe381d600d5c73b76e41227861d16bd49e6
JonatasMSS <jonatas.miguelss@gmail.com>
2023-02-19 00:00:31 -0300

    feat:created a framework of PersonAddressDialog

---
+++ src/components/dialogs/PersonAddressDialog.tsx
@@ -0,0 +1,23 @@
+
+
+import * as Dialog from "@radix-ui/react-dialog";
+
+export function PersonAddressDialog(){
+
+    return(
+        <Dialog.Root>
+            <Dialog.Trigger>
+
+            </Dialog.Trigger>
+            <Dialog.Portal>
+                <Dialog.Overlay/>
+                <Dialog.Title>
+
+                </Dialog.Title>
+                <Dialog.Content>
+                    
+                </Dialog.Content>
+            </Dialog.Portal>
+        </Dialog.Root>
+    )
+}
