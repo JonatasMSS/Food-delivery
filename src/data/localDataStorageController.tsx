b9752a302d6eff83d0717f3870b3f06bfe470ac4
JonatasMSS <jonatas.miguelss@gmail.com>
2023-02-26 22:26:54 -0300

    fixed: fixed the read of data from local storage

--- src/data/localDataStorageController.tsx
+++ src/data/localDataStorageController.tsx
@@ -4,39 +4,42 @@ import MainContext from "../components/context/MainContext";
 
 
 
-export class LocalStorageController{
+export class LocalStorageController {
     store: Storage;
-    setValueOfMainContext?:React.Dispatch<React.SetStateAction<number>> | undefined;
-    constructor(setValueUseState?:React.Dispatch<React.SetStateAction<number>> | undefined){
+    setValueOfMainContext?: React.Dispatch<React.SetStateAction<number>> | undefined;
+    constructor(setValueUseState?: React.Dispatch<React.SetStateAction<number>> | undefined) {
         this.store = localStorage;
         this.setValueOfMainContext = setValueUseState;
 
     }
-    
-     putDataInStorage(key:string,data:FoodToOrder[]):boolean{
+
+    putDataInStorage(key: string, data: FoodToOrder[]): boolean {
         this.setValueOfMainContext?.(data.length);
         const toStringData = JSON.stringify(data);
-        try {  
+        try {
             localStorage.setItem(key, toStringData);
             return true
         } catch (error) {
-            console.log('An error has ocurred:',error);
+            console.log('An error has ocurred:', error);
             return false;
         }
     }
-    getDataFromStorage(key:string):FoodToOrder[] | null{
-        const parsedData = JSON.parse(localStorage.getItem(key)!) as FoodToOrder[];
-       
-        this.setValueOfMainContext?.(parsedData.length);
-        return parsedData;
+    getDataFromStorage(key: string): FoodToOrder[] | null {
+        if (localStorage.getItem(key)) {
+            const parsedData = JSON.parse(localStorage.getItem(key)!) as FoodToOrder[];
+
+            this.setValueOfMainContext?.(parsedData.length);
+            return parsedData;
+        }
+        return null
     }
 
-    removeDataFromStorage(key:string,index:number){
-        const parsedData:FoodToOrder[] = JSON.parse(localStorage.getItem(key)!);
-        const newListWithRemovedByIndex = parsedData.filter((_,i) => i !== index);
-        
-        this.setValueOfMainContext?.(newListWithRemovedByIndex.length);
+    removeDataFromStorage(key: string, index: number) {
+        const parsedData: FoodToOrder[] = JSON.parse(localStorage.getItem(key)!);
+        const newListWithRemovedByIndex = parsedData.filter((_, i) => i !== index);
+
+        this.setValueOfMainContext!(newListWithRemovedByIndex.length);
         const convertToPut = JSON.stringify(newListWithRemovedByIndex);
-        localStorage.setItem(key,convertToPut);
+        localStorage.setItem(key, convertToPut);
     }
 }
